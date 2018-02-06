ad73df512301a9e305526c3ed6e55b26d7726e28
lixinrui706 <xli21@mail.csuchico.edu>
2018-02-05 18:03:22 -0800

    already compare which biggest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,28 @@
-//Author:
+//Author:Xinrui Li
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int a;
+  int b;
   cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  cin>>a;
 
+  cout<<"Please enter another whole number:\n";
+  cin>>b;
+  if(a <= b)
+  { 
+  
   cout<<"Of those two numbers, the biggest is: ";
+  cout<<b;
+  }
+  else
+  {
+  cout<<"Of those two numbers, the biggest is: ";
+  cout<<a;
+  }
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
